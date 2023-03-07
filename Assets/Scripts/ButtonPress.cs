b605dbc877033a2d1c30092b994a9420b32fdf7b
Gabryzzzzz <gabriele.monti24@gmail.com>
2023-03-08 00:46:27 +0100

    Tante cose

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public void move_object_on_press()
+    {
+        Debug.Log("Button Pressed");
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
