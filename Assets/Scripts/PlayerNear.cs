f819c8d9ea05446fb0aa6c02d24ac16a1af09659
Gabryzzzzz <gabriele.monti24@gmail.com>
2023-03-08 23:55:40 +0100

    Some UI Tests

--- Assets/Scripts/PlayerNear.cs
+++ Assets/Scripts/PlayerNear.cs
@@ -8,25 +8,26 @@ public class PlayerNear : MonoBehaviour
 
     public GameObject game_object;
     public GameObject player;
+    public Text text_test;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        text_test.text = "Player not is near";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(game_object.transform.position, player.transform.position) < 2.5f) 
+        if (Vector3.Distance(game_object.transform.position, player.transform.position) < 2.5f)
         {
-            if (Physics.Raycast(transform.position, transform.forward, out var hit, Mathf.Infinity))
-            {
-                var obj = hit.collider.gameObject;
-
-                Debug.Log($"looking at {obj.name}", this);
-            }
             Debug.Log("Player is near");
+            text_test.text = "Player is near";
         }
+        else 
+        {
+            text_test.text = "Player not is near";
+        }
+
     }
 }
