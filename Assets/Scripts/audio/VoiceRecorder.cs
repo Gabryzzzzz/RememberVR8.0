2b5db829042aea7d71c7fe80a747424e1ab5aed7
soulkira20 <andreatindaro.sciacca@gmail.com>
2023-03-23 09:09:33 +0100

    voice animation

--- Assets/Scripts/audio/VoiceRecorder.cs
+++ Assets/Scripts/audio/VoiceRecorder.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using TMPro;
 
     
 [RequireComponent(typeof(AudioSource))]
@@ -15,7 +16,8 @@ public class VoiceRecorder : MonoBehaviour
     private int maxFreq;
     public List <AudioClip> aud;
     public List <float> aud_lenght;
-    int i = 0;
+    public int i = 0;
+    public TMP_Text testo;
 
     public Stopwatch timer = new();
 
@@ -54,15 +56,45 @@ public class VoiceRecorder : MonoBehaviour
     private IEnumerator StopClipAfterSeconds() {
         yield return new WaitForSeconds(aud_lenght[i]);
         goAudioSource.Stop();
-        yield return null;
+        //testo.text = "";
     }
 
-    void OnGUI()
+    void Update()
     {
         //If there is a microphone    
         if (micConnected)
         {
+            //If the audio from any microphone isn't being captured    
+            if (!Microphone.IsRecording(null))
+            {
+                //Case the 'Record' button gets pressed    
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    //Start recording and store the audio captured from the microphone at the AudioClip in the AudioSource    
+                    //goAudioSource.clip = Microphone.Start(null, true, 20, maxFreq);
 
+                    StartCoroutine(StartRecording());
+                    aud_lenght.Add(0);
+                    timer.Start();
+                }
+            }
+            else //Recording is in progress    
+            {
+                testo.text = "Recording in progress...";
+                //Case the 'Stop and Play' button gets pressed    
+                if (Input.GetKeyDown(KeyCode.H))
+                {
+                    Microphone.End(null); //Stop the audio recording    
+                                          //goAudioSource.Play(); //Playback the recorded audio    
+
+                    testo.text = "Recording ended";
+                    timer.Stop();
+                    aud_lenght[aud.Count - 1] = (float)timer.Elapsed.TotalSeconds;
+                    timer.Reset();
+                }
+
+                //GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 25, 200, 50), "Recording in progress...");
+            }
 
             if (Input.GetKeyDown(KeyCode.X) && aud.Count > 0 && !goAudioSource.isPlaying)
             {
@@ -74,9 +106,11 @@ public class VoiceRecorder : MonoBehaviour
                 {
                     i++;
                 }
+
                 goAudioSource.clip = aud[i];
                 goAudioSource.Play();
-                StartCoroutine(nameof(StopClipAfterSeconds));
+                testo.text = "Playing clip:" +(i+1);
+                StartCoroutine(StopClipAfterSeconds());
                 //Debug.Log("aud[i] = " + aud[i].length);
                 //Debug.Log("print i: " + i);
                 //Debug.Log("aud.Count: " + aud.Count);
@@ -94,47 +128,19 @@ public class VoiceRecorder : MonoBehaviour
                 }
                 goAudioSource.clip = aud[i];
                 goAudioSource.Play();
-                StartCoroutine(nameof(StopClipAfterSeconds));
+                testo.text = "Playing clip:" +(i+1);
 
-            }
+                StartCoroutine(StopClipAfterSeconds());
 
-
-                //If the audio from any microphone isn't being captured    
-                if (!Microphone.IsRecording(null))
-            {
-                //Case the 'Record' button gets pressed    
-                if (Input.GetKeyDown(KeyCode.G))
-                {
-                    //Start recording and store the audio captured from the microphone at the AudioClip in the AudioSource    
-                    //goAudioSource.clip = Microphone.Start(null, true, 20, maxFreq);
-
-                    StartCoroutine(nameof(StartRecording));
-                    aud_lenght.Add(0);
-                    timer.Start();
-                }
-            }
-
-
-            else //Recording is in progress    
-            {
-                //Case the 'Stop and Play' button gets pressed    
-                if (Input.GetKeyDown(KeyCode.H))
-                {
-                    Microphone.End(null); //Stop the audio recording    
-                                          //goAudioSource.Play(); //Playback the recorded audio    
-                    timer.Stop();
-                    aud_lenght[aud.Count - 1] = (float)timer.Elapsed.TotalSeconds;
-                    timer.Reset();
-                }
-
-                GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 25, 200, 50), "Recording in progress...");
             }
         }
         else // No microphone    
         {
+
+            testo.text = "Microphone not connected!";
             //Print a red "Microphone not connected!" message at the center of the screen    
-            GUI.contentColor = Color.red;
-            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), "Microphone not connected!");
+            //GUI.contentColor = Color.red;
+            //GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), "Microphone not connected!");
         }
 
     }
@@ -143,4 +149,7 @@ public class VoiceRecorder : MonoBehaviour
         aud.Add(Microphone.Start(null, true, 20, maxFreq));
         yield return null;
     }
+
+
+
 }
